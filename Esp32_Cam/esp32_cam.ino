04e56e4fc48d49754d62486774738c8a90c9e15d
Rúben Garcia <2181459@my.ipleiria.pt>
2022-03-28 17:59:34 +0100

    Update arduino code

--- Esp32_Cam/esp32_cam.ino
+++ Esp32_Cam/esp32_cam.ino
@@ -8,44 +8,46 @@
 /*
   ESP.restart();
   ESP.eraseConfig()
-  
+
   #include <nvs_flash.h>
   nvs_flash_init();
   nvs_flash_erase();
-  
+
 */
 
-void setupSerial() {
+static void setupSerial() {
   Serial.begin(SERIAL_BAUD);
   Serial.setDebugOutput(true);
 }
 
-bool setupWifi() {
-  WiFiManager wifiManager;
-  
+static bool setupWifi() {
+  static WiFiManager wifiManager;
+
   wifiManager.debugPlatformInfo();
   wifiManager.setDarkMode(true);
 
   Serial.println("Starting WiFiManager...");
   if (!wifiManager.autoConnect(ACCESS_POINT_NAME)) {
-        Serial.println("Failed to connect to WiFi.");
+    Serial.println("Failed to connect to WiFi.");
     return false;
-    
+
   }
-  
+
   Serial.println("Successfully connected to WiFi.");
-    return true;
+  return true;
 }
 
-bool initialized = false;
+static bool initialized = false;
 void setup() {
+  Serial.println("SETUP STARTED");
   setupSerial();
-  
+
   if (!setupWifi()) {
     Serial.println("Restarting ESP...");
     ESP.restart();
     return;
   }
 
+  setupCamera();
   initialized = true;
 }
--- Esp32_Cam/esp32_cam_camera.ino
+++ Esp32_Cam/esp32_cam_camera.ino
@@ -1,8 +1,8 @@
 #include "esp_camera.h"
 
-camera_config_t camera_config;
+static camera_config_t camera_config;
 
-void cameraSettings() {
+static void setupCamera() {
   camera_config.ledc_channel = LEDC_CHANNEL_0;
   camera_config.ledc_timer = LEDC_TIMER_0;
   camera_config.pin_d0 = 5;
@@ -28,10 +28,43 @@ void cameraSettings() {
   camera_config.fb_count = 2;
 }
 
-void s() {
+static bool startCamera() {
   esp_err_t err = esp_camera_init(&camera_config);
   if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
+    Serial.printf("Fail to start camera error 0x%x", err);
+    return false;
+  }
+
+  return true;
+}
+
+static void enable_flash(bool enable) {
+/*
+  ledc_set_duty(CONFIG_LED_LEDC_SPEED_MODE, CONFIG_LED_LEDC_CHANNEL, CONFIG_LED_MAX_INTENSITY);
+  ledc_update_duty(CONFIG_LED_LEDC_SPEED_MODE, CONFIG_LED_LEDC_CHANNEL);
+  CONFIG_LED_LEDC_LOW_SPEED_MODE
+  LEDC_LOW_SPEED_MODE
+  LEDC_HIGH_SPEED_MODE
+
+  vTaskDelay(150 / portTICK_PERIOD_MS); // The LED needs to be turned on ~150ms before the call to esp_camera_fb_get()
+  */
+}
+
+static void captureCamera() {
+  sensor_t * sensor = esp_camera_sensor_get();
+  if (sensor != NULL) {
     return;
   }
+  
+  if (sensor->id.PID == OV3660_PID) {
+    sensor->set_vflip(sensor, 1); // flip it back
+    sensor->set_brightness(sensor, 1); // up the brightness just a bit
+    sensor->set_saturation(sensor, -2); // lower the saturation
+  }
+
+  sensor->set_framesize(sensor, FRAMESIZE_QVGA);
+  Serial.println(sensor->id.PID);
+  Serial.println(OV3660_PID);
+  Serial.println("Capture");
+  Serial.println("Capture2");
 }
