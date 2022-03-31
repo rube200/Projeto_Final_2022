0b28bb7d6e99f5ff065d3992f054be8a9a877840
Rúben Garcia <2181459@my.ipleiria.pt>
2022-03-31 16:03:34 +0100

    Update Server.py

--- Experimental_Project/Server.py
+++ Experimental_Project/Server.py
@@ -1,16 +1,19 @@
+import os
 import selectors
 import socket
 import traceback
+from threading import Thread
 
 import pandas
 from flask import Flask, render_template
 
 from message import Message
 
-HOST = "0.0.0.0"
+NAME = 'Video-Doorbell'
+HOST = '0.0.0.0'
 PORT = 45000
 
-app = Flask(__name__)
+app = Flask(NAME)
 
 
 @app.route('/')
@@ -48,53 +51,61 @@ def stats():
 
     html = df.to_html()
     # Func = open('templates/stats.html','a')
-    # Func.write("\n<a href=\"{{ url_for('index') }}\">Return To Homepage</a>\n</body>")
+    # Func.write('\n<a href=\'{{ url_for('index') }}\'>Return To Homepage</a>\n</body>')
     return render_template('stats.html')
 
 
-@app.route("/stream")
+@app.route('/stream')
 def stream():
     pass
 
 
-def accept_socket_client(sv_socket):
-    connection, address = sv_socket.accept()
-    print(f"Accepted a connection from {address}")
+def accept_socket_client(selector, sv_socket):
+    try:
+        connection, address = sv_socket.accept()
+        print(f'Accepted a connection from {address}')
 
-    connection.setblocking(False)
-    message = Message(selector, connection, address)
-    selector.register(connection, selectors.EVENT_READ, data=message)
+        connection.setblocking(False)
+        message = Message(selector, connection, address)
+        selector.register(connection, selectors.EVENT_READ, data=message)
+    except Exception as ex:
+        print(f'Exception while accepting new socket client: {ex!r}')
+        print(f'{traceback.format_exc()}')
 
 
-def socket_server():
-    with socket.socket() as sv_socket:
-        try:
+def socket_server(selector):
+    while True:
+        events = selector.select()
+        for key, mask in events:
+            if key.data:
+                message = key.data
+                try:
+                    key.data.process_events(mask)
+                except Exception as ex:
+                    print(f'Exception while processing event for {message.client_address}: {ex!r}')
+                    print(f'{traceback.format_exc()}')
+                    message.close()
+            else:
+                # noinspection PyTypeChecker
+                accept_socket_client(selector, key.fileobj)
+
+
+def start_socket_server():
+    try:
+        with selectors.DefaultSelector() as selector, socket.socket() as sv_socket:
             sv_socket.bind((HOST, PORT))
             sv_socket.listen()
             sv_socket.setblocking(False)
 
             selector.register(sv_socket, selectors.EVENT_READ)
-            print("Server ready! Waiting connections...")
-            while True:
-                events = selector.select()
-                for key, mask in events:
-                    if key.data:
-                        message = key.data
-                        try:
-                            key.data.process_events(mask)
-                        except Exception as ex:
-                            print(f"Exception while processing event for {message.client_address}: {ex!r}")
-                            print(f"{traceback.format_exc()}")
-                            message.close()
-                    else:
-                        # noinspection PyTypeChecker
-                        accept_socket_client(key.fileobj)
-
-        except KeyboardInterrupt:
-            print("Caught keyboard interrupt, exiting...")
+            print('Socket ready! Waiting connections...')
+            socket_server(selector)
+    except Exception as ex:
+        print(f'Exception while starting socket server: {ex!r}')
+        print(f'{traceback.format_exc()}')
 
 
-if __name__ == '__main__':
-    with selectors.DefaultSelector() as selector:
-        socket_server()
+if __name__ == '__main__' and not os.environ.get('WERKZEUG_RUN_MAIN'):
+    socket_thread = Thread(target=start_socket_server)
+    socket_thread.start()
     app.run(debug=True, host='0.0.0.0', port=80)
