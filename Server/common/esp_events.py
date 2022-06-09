36e20e66542be1e5397eb0e2ee2e2320c00aa4b6
Rúben Garcia <ru_dani@hotmail.com>
2022-06-09 04:32:52 +0100

    Fix record and costume event system

--- Server/common/esp_events.py
+++ Server/common/esp_events.py
@@ -1,9 +1,48 @@
-from events import Events
+from typing import Callable, Any
 
 
-class EspEvents(Events):
+# Based on events.py
+class EventSlot:
+    def __init__(self, name):
+        self.__name__ = name
+        self.funcs = []
+
+    def __repr__(self):
+        return f'Event: {self.__name__}'
+
+    def __call__(self, *args, **kwargs):
+        for f in list(self.funcs):
+            result = f(*args, **kwargs)
+            if result:
+                return result
+        return None
+
+    def __iadd__(self, func: Callable[[Any], Any]):
+        self.funcs.append(func)
+        return self
+
+    def __isub__(self, func: Callable[[Any], Any]):
+        while func in self.funcs:
+            self.funcs.remove(func)
+        return self
+
+    def __len__(self):
+        return len(self.funcs)
+
+    def __iter__(self):
+        def gen():
+            for func in self.funcs:
+                yield func
+
+        return gen()
+
+    def __getitem__(self, key):
+        return self.funcs[key]
+
+
+class EspEvents:
     def __init__(self):
-        super(EspEvents, self).__init__((
+        self.__events__ = (
             'on_esp_uuid_recv',  # Client -> (username, bell_duration, motion_duration)
             'on_esp_username_recv',  # Client, username -> bool
             'on_notification',  # Client, NotificationType, Path -> None
@@ -11,4 +50,27 @@ class EspEvents(Events):
             'on_open_doorbell_requested',  # uuid -> None
             'on_start_stream_requested',  # uuid, is_maintain_stream -> None
             'on_stop_stream_requested'  # uuid -> None
-        ))
+        )
+
+    def __getattr__(self, name: str):
+        if name not in self.__events__:
+            raise AttributeError(f'\'{name}\' is not a declared event')
+
+        self.__dict__[name] = ev = EventSlot(name)
+        return ev
+
+    def __repr__(self):
+        return f'<{self.__class__.__module__}.{self.__class__.__name__} object at {hex(id(self))}>'
+
+    __str__ = __repr__
+
+    def __len__(self):
+        return len(list(self.__iter__()))
+
+    def __iter__(self):
+        def gen(dic=self.__dict__.items()):
+            for attr, val in dic:
+                if isinstance(val, EventSlot):
+                    yield val
+
+        return gen()
--- Server/socket_client/client_record.py
+++ Server/socket_client/client_record.py
@@ -2,7 +2,8 @@ from threading import Thread, Event
 from time import sleep, monotonic
 from typing import Callable
 
-from cv2 import VideoWriter, VideoWriter_fourcc
+import numpy
+from cv2 import imdecode, IMREAD_COLOR, VideoWriter, VideoWriter_fourcc
 
 FOURCC = VideoWriter_fourcc(*'MP4V')
 
@@ -10,24 +11,26 @@ FOURCC = VideoWriter_fourcc(*'MP4V')
 class ClientRecord(Thread):
     def __init__(self, get_camera: Callable[[], bytes]):
         super(ClientRecord, self).__init__()
-        self.__finished = Event()
         self.__get_camera = get_camera
         self.__interval = 0.05
         self.__record_until = 0.0
+        self.__reset = Event()
         self.__running = False
         self.__video_writer = None
         self.__video_name = None
+        self.__waiter = Event()
         self.__working = False
 
     def __del__(self):
         self.stop()
         sleep(0.01)
-        del self.__finished
         del self.__get_camera
         del self.__interval
         del self.__record_until
+        del self.__reset
         del self.__video_name
         del self.__video_writer
+        del self.__waiter
 
     def start_record(self, name: str, record_until: float) -> str or None:
         if self.__working:
@@ -42,8 +45,9 @@ class ClientRecord(Thread):
         self.__video_writer = VideoWriter(name, FOURCC, 20, (320, 240))
 
         if self.__running:
-            self.__finished.set()
-            self.__finished.clear()
+            self.__waiter.clear()
+            self.__reset.set()
+            self.__reset.clear()
             return self.__video_name
 
         self.__running = True
@@ -53,31 +57,35 @@ class ClientRecord(Thread):
     def __take_frame(self):
         frame = self.__get_camera()
         if frame and self.__video_writer:
-            self.__video_writer.write(frame)
+            np_img = numpy.frombuffer(frame, dtype=numpy.uint8)
+            img = imdecode(np_img, IMREAD_COLOR)
+            self.__video_writer.write(img)
             return
 
         self.__working = False
 
     def __process_record(self):
-        while self.__working:
-            if self.__finished.wait(self.__interval) or self.__record_until < monotonic():
-                break
+        try:
+            self.__working = True
+            while self.__working:
+                if self.__waiter.wait(self.__interval) or self.__record_until < monotonic():
+                    break
 
-            self.__take_frame()
+                self.__take_frame()
 
-        if self.__video_writer:
-            self.__video_name = None
-            self.__video_writer.release()
-            self.__video_writer = None
+            if self.__video_writer:
+                self.__video_name = None
+                self.__video_writer.release()
+                self.__video_writer = None
+        finally:
+            self.__working = False
 
     def run(self):
-        self.__working = True
         while self.__running:
             self.__process_record()
-            self.__finished.wait()
-            sleep(0.01)
+            self.__reset.wait()
 
     def stop(self):
         self.__running = False
         self.__working = False
-        self.__finished.set()
+        self.__waiter.set()
