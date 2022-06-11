e2f08cedbbd010bc50bdcc65314c013747c19447
Rúben Garcia <ru_dani@hotmail.com>
2022-06-11 22:31:18 +0100

    Update static files

---
+++ Server/web/static/theme.js
@@ -0,0 +1,28 @@
+(function () {
+    "use strict"; // Start of use strict
+
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarToggles = document.querySelectorAll('#sidebarToggle, #sidebarToggleTop');
+    if (sidebar) {
+        for (const toggle of sidebarToggles) {
+            // Toggle the side navigation
+            toggle.addEventListener('click', function (e) {
+                document.body.classList.toggle('sidebar-toggled');
+                sidebar.classList.toggle('toggled');
+            });
+        }
+    }
+
+    const scrollToTop = document.querySelector('.scroll-to-top');
+    if (scrollToTop) {
+        window.addEventListener('scroll', function () {
+            const scrollDistance = window.scrollY;
+            if (scrollDistance > 100) {
+                scrollToTop.style.display = 'block';
+            } else {
+                scrollToTop.style.display = 'none';
+            }
+        });
+    }
+
+})(); // End of use strict
