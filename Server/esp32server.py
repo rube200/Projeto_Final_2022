44331ede2c8cb42023b94c4c0f609bc0735bcc4a
Rúben Garcia <ru_dani@hotmail.com>
2022-06-15 17:14:27 +0100

    Fixes related to fcgi

--- Server/esp32server.py
+++ Server/esp32server.py
@@ -29,15 +29,16 @@ def config_logger(filename: str = 'esp32server.py.log'):
 
 
 class Esp32Server:
-    def __init__(self):
+    def __init__(self, fcgi: bool = True):
         self.__clients = EspClients()
         self.__events = EspEvents()
+        self.__fcgi = fcgi
         self.__socket_server = None
         self.__socket_thread = None
         self.__web_server = WebServer(self.__clients, self.__events)
 
     def __del__(self):
-        if 'WERKZEUG_RUN_MAIN' in environ or not environ.get('FLASK_DEBUG'):
+        if 'WERKZEUG_RUN_MAIN' in environ or self.__fcgi:
             log.info('Stopping Servers...')
 
         if self.__socket_server:
@@ -52,15 +53,15 @@ class Esp32Server:
         del self.__events
         del self.__web_server
 
-    def run_servers(self, fcgi: bool = True):
-        if 'WERKZEUG_RUN_MAIN' in environ or not environ.get('FLASK_DEBUG'):
+    def run_servers(self):
+        if 'WERKZEUG_RUN_MAIN' in environ or self.__fcgi:
             log.info('Running Servers...')
             self.__socket_server = ServerSocket(self.__clients, self.__events)
             self.__socket_thread = Thread(daemon=True, target=self.__socket_server.run_forever)
             self.__socket_thread.start()
 
-        if not fcgi:
-            self.__web_server.run(port=environ.get('FLASK_PORT'))
+        if not self.__fcgi:
+            self.__web_server.run(port=80)
 
     @property
     def web(self) -> WebServer:
@@ -69,8 +70,8 @@ class Esp32Server:
 
 def main():
     try:
-        esp32server = Esp32Server()
-        esp32server.run_servers(False)
+        esp32server = Esp32Server(False)
+        esp32server.run_servers()
     except Exception as ex:
         log.error(f'Exception while initializing Esp32Server: {ex!r}')
         log.error(format_exc())
