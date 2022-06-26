33dc08547b6bb4cab29536a76710a6e70296ed83
Rúben Garcia <ru_dani@hotmail.com>
2022-06-26 15:05:15 +0100

    Fix sidebar toggler

--- Server/web/static/theme.js
+++ Server/web/static/theme.js
@@ -3,19 +3,41 @@
 
     const sidebar = document.querySelector('.sidebar');
     const sidebarToggles = document.querySelectorAll('#sidebarToggle, #sidebarToggleTop');
+    let userToggle = false;
     if (sidebar) {
         for (const toggle of sidebarToggles) {
             // Toggle the side navigation
-            toggle.addEventListener('click', function () {
+            toggle.addEventListener('click', () => {
                 document.body.classList.toggle('sidebar-toggled');
                 sidebar.classList.toggle('toggled');
+                userToggle = sidebar.classList.contains('toggled');
             });
         }
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth < 629) {
+                if (!userToggle) {
+                    document.body.classList.add('sidebar-toggled');
+                    sidebar.classList.add('toggled');
+                }
+                return;
+            }
+
+            if (userToggle) {
+                document.body.classList.add('sidebar-toggled');
+                sidebar.classList.add('toggled');
+
+            }
+            else {
+                document.body.classList.remove('sidebar-toggled');
+                sidebar.classList.remove('toggled');
+            }
+        });
     }
 
     const scrollToTop = document.querySelector('.scroll-to-top');
     if (scrollToTop) {
-        window.addEventListener('scroll', function () {
+        window.addEventListener('scroll', () => {
             const scrollDistance = window.scrollY;
             if (scrollDistance > 100) {
                 scrollToTop.style.display = 'block';
